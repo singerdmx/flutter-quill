c2bbf2d7936c632eed79efa7045640fa14cfbaec
Xin Yao <singerdmx@gmail.com>
2021-02-28 02:39:08 -0800

    Expand link prefixes
    https://beradrian.wordpress.com/2010/01/15/special-links/

--- lib/widgets/editor.dart
+++ lib/widgets/editor.dart
@@ -35,6 +35,16 @@ const linkPrefixes = [
   'mailto:', // email
   'tel:', // telephone
   'sms:', // SMS
+  'callto:',
+  'wtai:',
+  'market:',
+  'geopoint:',
+  'ymsgr:',
+  'msnim:',
+  'gtalk:', // Google Talk
+  'skype:',
+  'sip:', // Lync
+  'whatsapp:',
   'http'
 ];
 
