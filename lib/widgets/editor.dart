60c755d86d01e2321953178c4ac96b105f8a622c
Xin Yao <singerdmx@gmail.com>
2021-03-26 20:28:03 -0700

    Revert "Added QuillEditor Key (#113)"
    This reverts commit bdb8f1c22f722601f0842e916129bcea38465eb1.

--- lib/widgets/editor.dart
+++ lib/widgets/editor.dart
@@ -189,9 +189,7 @@ class QuillEditor extends StatefulWidget {
       this.scrollPhysics,
       this.onLaunchUrl,
       this.embedBuilder =
-          kIsWeb ? _defaultEmbedBuilderWeb : _defaultEmbedBuilder,
-      Key? key,
-      }) : super(key: key)
+          kIsWeb ? _defaultEmbedBuilderWeb : _defaultEmbedBuilder});
 
   factory QuillEditor.basic(
       {required QuillController controller, required bool readOnly}) {
