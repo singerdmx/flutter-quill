9f689306956f202e759f4c850a90db89c0d164fb
Andy Trand <magtuxgit@gmail.com>
2021-12-07 17:57:09 +0200

    move cursor helpers (#507)

--- lib/src/widgets/controller.dart
+++ lib/src/widgets/controller.dart
@@ -225,6 +225,16 @@ class QuillController extends ChangeNotifier {
     formatText(selection.start, selection.end - selection.start, attribute);
   }
 
+  void moveCursorToStart() {
+    updateSelection(TextSelection.collapsed(offset: 0), ChangeSource.LOCAL);
+  }
+
+  void moveCursorToEnd() {
+    updateSelection(
+        TextSelection.collapsed(offset: plainTextEditingValue.text.length),
+        ChangeSource.LOCAL);
+  }
+
   void updateSelection(TextSelection textSelection, ChangeSource source) {
     _updateSelection(textSelection, source);
     notifyListeners();
