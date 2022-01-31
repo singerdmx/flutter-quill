80ff3d312393bfd782241360a77c2d0d4903d919
Horst Haag <horst.haag@me.com>
2022-01-31 08:24:59 +0100

    export style class

--- lib/flutter_quill.dart
+++ lib/flutter_quill.dart
@@ -4,6 +4,7 @@ export 'src/models/documents/attribute.dart';
 export 'src/models/documents/document.dart';
 export 'src/models/documents/nodes/embeddable.dart';
 export 'src/models/documents/nodes/leaf.dart';
+export 'src/models/documents/style.dart';
 export 'src/models/quill_delta.dart';
 export 'src/models/themes/quill_dialog_theme.dart';
 export 'src/models/themes/quill_icon_theme.dart';
