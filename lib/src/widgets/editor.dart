f6f548f5482a521899b3f11b37ecbb7d76b64515
friebetill <friebetill@gmail.com>
2022-09-16 09:31:29 +0200

    Add typed_data import for Uint8List
    Without the import, the project cannot be compiled.

--- lib/src/widgets/editor.dart
+++ lib/src/widgets/editor.dart
@@ -1,4 +1,5 @@
 import 'dart:math' as math;
+import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
--- lib/src/widgets/raw_editor.dart
+++ lib/src/widgets/raw_editor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math' as math;
+import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
