95b8f24a8dbed0617c0f152211425abcde6d1858
X Code <singerdmx@gmail.com>
2023-04-02 09:03:45 -0700

    Format code

--- lib/src/widgets/raw_editor.dart
+++ lib/src/widgets/raw_editor.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:ui' as ui hide TextStyle;
 import 'dart:math' as math;
 // ignore: unnecessary_import
 import 'dart:typed_data';
+import 'dart:ui' as ui hide TextStyle;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
