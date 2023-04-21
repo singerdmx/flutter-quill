c4f5d6e74a219c9f94d7160e5abb67b56724399c
bambinoua <130981115+MacDeveloper1@users.noreply.github.com>
2023-04-21 09:55:08 +0200

    Export `UtilityWidgets` via flutter_quill.extensions

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -4,3 +4,4 @@ export 'src/models/documents/nodes/leaf.dart' hide Text;
 export 'src/models/rules/insert.dart';
 export 'src/utils/platform.dart';
 export 'src/utils/string.dart';
+export 'src/utils/widgets.dart';
