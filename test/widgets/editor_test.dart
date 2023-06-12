a9d5631f27638d29f786630518dfdef1df316d8b
Richard Marshall <richard@unrelated.dev>
2023-06-12 16:20:25 -0700

    Add test for Android keyboard image insertion

--- test/widgets/editor_test.dart
+++ test/widgets/editor_test.dart
@@ -1,4 +1,7 @@
+import 'dart:convert' show jsonDecode;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_quill/src/widgets/controller.dart';
 import 'package:flutter_quill/src/widgets/editor.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -26,6 +29,56 @@ void main() {
       await tester.quillEnterText(find.byType(QuillEditor), 'test\n');
 
       expect(controller.document.toPlainText(), 'test\n');
+    });
+
+    testWidgets('insertContent is handled correctly', (tester) async {
+      String? latestUri;
+      await tester.pumpWidget(
+        MaterialApp(
+          home: QuillEditor(
+            controller: controller,
+            focusNode: FocusNode(),
+            scrollController: ScrollController(),
+            scrollable: true,
+            padding: const EdgeInsets.all(0),
+            autoFocus: true,
+            readOnly: false,
+            expands: true,
+            contentInsertionConfiguration: ContentInsertionConfiguration(
+              onContentInserted: (content) {
+                latestUri = content.uri;
+              },
+              allowedMimeTypes: const <String>['image/gif'],
+            ),
+          ),
+        ),
+      );
+      await tester.tap(find.byType(QuillEditor));
+      await tester.quillEnterText(find.byType(QuillEditor), 'test\n');
+      await tester.idle();
+
+      const uri =
+          'content://com.google.android.inputmethod.latin.fileprovider/test.gif';
+      final messageBytes =
+          const JSONMessageCodec().encodeMessage(<String, dynamic>{
+        'args': <dynamic>[
+          -1,
+          'TextInputAction.commitContent',
+          jsonDecode(
+              '{"mimeType": "image/gif", "data": [0,1,0,1,0,1,0,0,0], "uri": "$uri"}'),
+        ],
+        'method': 'TextInputClient.performAction',
+      });
+
+      Object? error;
+      try {
+        await tester.binding.defaultBinaryMessenger
+            .handlePlatformMessage('flutter/textinput', messageBytes, (_) {});
+      } catch (e) {
+        error = e;
+      }
+      expect(error, isNull);
+      expect(latestUri, equals(uri));
     });
   });
 }
