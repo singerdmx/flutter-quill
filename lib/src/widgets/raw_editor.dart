bed3787aa9a2920890b51edc07c6ae56c1c0af03
Vladimir <37017173+VladimirKaroyan@users.noreply.github.com>
2023-08-16 22:20:30 +0400

    Update raw_editor.dart for compatibility with Flutter version 3.13.0

--- lib/src/widgets/raw_editor.dart
+++ lib/src/widgets/raw_editor.dart
@@ -350,6 +350,7 @@ class RawEditorState extends EditorState
   List<ContextMenuButtonItem> get contextMenuButtonItems {
     return EditableText.getEditableButtonItems(
       clipboardStatus: _clipboardStatus.value,
+      onLiveTextInput: null,
       onCopy: copyEnabled
           ? () => copySelection(SelectionChangedCause.toolbar)
           : null,
