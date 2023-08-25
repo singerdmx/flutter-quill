a0a5cbadec221122957e84e8cb8610188ddf5cd7
EgorK0rshun <egork@digitalchemy.us>
2023-08-25 14:55:53 +0300

    Rename notInlineStyle to ignoredStyles

--- lib/src/widgets/controller.dart
+++ lib/src/widgets/controller.dart
@@ -400,10 +400,10 @@ class QuillController extends ChangeNotifier {
         extentOffset: math.min(selection.extentOffset, end));
     if (_keepStyleOnNewLine) {
       final style = getSelectionStyle();
-      final notInlineStyle = style.attributes.values.where(
+      final ignoredStyles = style.attributes.values.where(
         (s) => !s.isInline || s.key == Attribute.link.key,
       );
-      toggledStyle = style.removeAll(notInlineStyle.toSet());
+      toggledStyle = style.removeAll(ignoredStyles.toSet());
     } else {
       toggledStyle = Style();
     }
