97c974c4546259ea1fc707e3e38fbcd4986df948
Ahmed Hnewa <73608287+freshtechtips@users.noreply.github.com>
2023-10-24 19:28:03 +0300

    Forgot to run ./before-push

--- lib/src/widgets/toolbar/base_toolbar.dart
+++ lib/src/widgets/toolbar/base_toolbar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 
 import '../../../flutter_quill.dart';
-import '../../models/config/toolbar/base_configurations.dart';
+// import '../../models/config/toolbar/base_configurations.dart';
 import '../../utils/extensions/build_context.dart';
 import 'buttons/arrow_indicated_list.dart';
 
