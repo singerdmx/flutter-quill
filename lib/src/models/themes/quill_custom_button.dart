0632090f4407aa86bf0d447366feb38db306c62d
Ellet <73608287+freshtechtips@users.noreply.github.com>
2023-11-09 20:11:33 +0300

    Update QuillCustomButton

--- lib/src/models/themes/quill_custom_button.dart
+++ lib/src/models/themes/quill_custom_button.dart
@@ -2,6 +2,7 @@
 
 // import '../../widgets/toolbar/base_toolbar.dart';
 
+<<<<<<< HEAD
 // class QuillCustomButton extends QuillToolbarBaseButtonOptions {
 //   const QuillCustomButton({
 //     this.icon,
@@ -20,3 +21,23 @@
 //   /// The customButton placeholder
 //   final Widget? child;
 // }
+=======
+class QuillCustomButton extends QuillToolbarBaseButtonOptions {
+  const QuillCustomButton({
+    this.icon,
+    this.onTap,
+    super.tooltip,
+    this.child,
+    super.iconTheme,
+  });
+
+  /// The icon widget
+  final Widget? icon;
+
+  /// The function when the icon is tapped
+  final VoidCallback? onTap;
+
+  /// The customButton placeholder
+  final Widget? child;
+}
+>>>>>>> ad642da (Update QuillCustomButton)
