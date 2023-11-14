a2a0378d46b9ef7d44682bb21dd80fd5ca60f2b5
Ellet <hello@freshplatform.net>
2023-11-14 03:09:22 +0300

    Make DocChange constant

--- lib/src/models/structs/doc_change.dart
+++ lib/src/models/structs/doc_change.dart
@@ -1,8 +1,11 @@
+import 'package:meta/meta.dart' show immutable;
+
 import '../documents/document.dart';
 import '../quill_delta.dart';
 
+@immutable
 class DocChange {
-  DocChange(
+  const DocChange(
     this.before,
     this.change,
     this.source,
