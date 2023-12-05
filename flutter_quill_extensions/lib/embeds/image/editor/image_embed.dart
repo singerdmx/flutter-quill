2cb886bcd166270a8969035172b1bf784b84da8d
Ellet <hello@freshplatform.net>
2023-12-05 13:24:18 +0300

    Fix analysis warrning

--- flutter_quill_extensions/lib/embeds/image/editor/image_embed.dart
+++ flutter_quill_extensions/lib/embeds/image/editor/image_embed.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' hide OptionalSize;
 import 'package:flutter_quill/translations.dart';
