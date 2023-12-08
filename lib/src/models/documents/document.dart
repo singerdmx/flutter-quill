d657731e18e96c1dbd45ea4aa2a84ac3e974fbc4
Ellet <hello@freshplatform.net>
2023-12-08 10:20:05 +0300

    Fix analysis warrning

--- lib/src/models/documents/document.dart
+++ lib/src/models/documents/document.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
+import 'dart:async' show StreamController;
 
-import '../../widgets/others/cursor.dart';
 import '../../widgets/quill/embeds.dart';
 import '../quill_delta.dart';
 import '../rules/rule.dart';
