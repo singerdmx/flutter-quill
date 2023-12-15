4052779cfe7d66dc2de9b880ef1b4d871e846e21
Ellet <hello@freshplatform.net>
2023-12-15 14:19:16 +0300

    Update delta_to_markdown

--- lib/src/packages/quill_markdown/delta_to_markdown.dart
+++ lib/src/packages/quill_markdown/delta_to_markdown.dart
@@ -310,15 +310,15 @@ abstract class _NodeVisitor<T> {
 extension _NodeX on Node {
   T accept<T>(_NodeVisitor<T> visitor, [T? context]) {
     switch (runtimeType) {
-      case Root:
+      case const (Root):
         return visitor.visitRoot(this as Root, context);
-      case Block:
+      case const (Block):
         return visitor.visitBlock(this as Block, context);
-      case Line:
+      case const (Line):
         return visitor.visitLine(this as Line, context);
-      case QuillText:
+      case const (QuillText):
         return visitor.visitText(this as QuillText, context);
-      case Embed:
+      case const (Embed):
         return visitor.visitEmbed(this as Embed, context);
     }
     throw Exception('Container of type $runtimeType cannot be visited');
