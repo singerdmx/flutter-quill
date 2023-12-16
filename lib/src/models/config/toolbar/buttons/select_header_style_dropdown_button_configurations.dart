41b2483f90eb815431f9bbccf62f4e8354b6ebf7
Ellet <hello@freshplatform.net>
2023-12-16 23:47:19 +0300

    Remove unused properties in the new select header dropdown button

--- lib/src/models/config/toolbar/buttons/select_header_style_dropdown_button_configurations.dart
+++ lib/src/models/config/toolbar/buttons/select_header_style_dropdown_button_configurations.dart
@@ -1,14 +1,5 @@
-import 'package:flutter/material.dart' show PopupMenuEntry;
 import 'package:flutter/widgets.dart'
-    show
-        Color,
-        EdgeInsets,
-        EdgeInsetsGeometry,
-        IconData,
-        TextOverflow,
-        TextStyle,
-        ValueChanged,
-        VoidCallback;
+    show IconData, TextStyle, ValueChanged, VoidCallback;
 
 import '../../../../widgets/toolbar/base_toolbar.dart';
 import '../../../documents/attribute.dart';
@@ -38,30 +29,13 @@ class QuillToolbarSelectHeaderStyleDropdownButtonOptions
     this.iconButtonFactor,
     this.textStyle,
     super.iconData,
-    this.fillColor,
-    this.hoverElevation = 1,
-    this.highlightElevation = 1,
-    this.onSelected,
     this.attributes,
-    this.padding,
-    this.style,
-    this.width,
-    this.labelOverflow = TextOverflow.visible,
-    this.itemHeight,
-    this.itemPadding,
-    this.defaultItemColor,
-    this.renderItemTextStyle = false,
   });
 
   /// By default we will the toolbar axis from [QuillSimpleToolbarConfigurations]
   final double? iconSize;
   final double? iconButtonFactor;
   final TextStyle? textStyle;
-
-  final Color? fillColor;
-  final double hoverElevation;
-  final double highlightElevation;
-  final ValueChanged<String>? onSelected;
 
   /// Header attributes, defaults to:
   /// ```dart
@@ -76,38 +50,17 @@ class QuillToolbarSelectHeaderStyleDropdownButtonOptions
   /// ]
   /// ```
   final List<Attribute<int>>? attributes;
-  final EdgeInsetsGeometry? padding;
-  final TextStyle? style;
-  final double? width;
-  final TextOverflow labelOverflow;
-  final double? itemHeight;
-  final EdgeInsets? itemPadding;
-  final Color? defaultItemColor;
-  final bool renderItemTextStyle;
 
   QuillToolbarSelectHeaderStyleDropdownButtonOptions copyWith({
-    Color? fillColor,
-    double? hoverElevation,
-    double? highlightElevation,
-    List<PopupMenuEntry<String>>? items,
     ValueChanged<String>? onSelected,
     List<Attribute<int>>? attributes,
-    EdgeInsetsGeometry? padding,
     TextStyle? style,
-    double? width,
-    TextOverflow? labelOverflow,
-    bool? renderFontFamilies,
-    bool? overrideTooltipByFontFamily,
-    double? itemHeight,
-    EdgeInsets? itemPadding,
-    Color? defaultItemColor,
     double? iconSize,
     double? iconButtonFactor,
     IconData? iconData,
     VoidCallback? afterButtonPressed,
     String? tooltip,
     QuillIconTheme? iconTheme,
-    bool? renderItemTextStyle,
   }) {
     return QuillToolbarSelectHeaderStyleDropdownButtonOptions(
       attributes: attributes ?? this.attributes,
@@ -115,20 +68,8 @@ class QuillToolbarSelectHeaderStyleDropdownButtonOptions
       afterButtonPressed: afterButtonPressed ?? this.afterButtonPressed,
       tooltip: tooltip ?? this.tooltip,
       iconTheme: iconTheme ?? this.iconTheme,
-      onSelected: onSelected ?? this.onSelected,
-      padding: padding ?? this.padding,
-      style: style ?? this.style,
-      width: width ?? this.width,
-      labelOverflow: labelOverflow ?? this.labelOverflow,
-      itemHeight: itemHeight ?? this.itemHeight,
-      itemPadding: itemPadding ?? this.itemPadding,
-      defaultItemColor: defaultItemColor ?? this.defaultItemColor,
       iconSize: iconSize ?? this.iconSize,
       iconButtonFactor: iconButtonFactor ?? this.iconButtonFactor,
-      fillColor: fillColor ?? this.fillColor,
-      hoverElevation: hoverElevation ?? this.hoverElevation,
-      highlightElevation: highlightElevation ?? this.highlightElevation,
-      renderItemTextStyle: renderItemTextStyle ?? this.renderItemTextStyle,
     );
   }
 }
