448050f3d915c17311a2db0e2442ce58e0249886
Aleksei <130981115+MacDeveloper1@users.noreply.github.com>
2023-12-20 10:46:30 +0100

    Export `QuillToolbarSelectAlignmentButtons`

--- lib/src/widgets/toolbar/base_toolbar.dart
+++ lib/src/widgets/toolbar/base_toolbar.dart
@@ -9,6 +9,7 @@ import 'simple_toolbar.dart';
 
 export '../../models/config/toolbar/base_button_configurations.dart';
 export '../../models/config/toolbar/simple_toolbar_configurations.dart';
+export 'buttons/alignment/select_alignment_button.dart';
 export 'buttons/alignment/select_alignment_button_original.dart';
 export 'buttons/clear_format_button.dart';
 export 'buttons/color/color_button.dart';
