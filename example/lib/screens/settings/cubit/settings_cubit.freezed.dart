c2c4671f281ff986ff0cbff105e7aaa1d87a148f
Ellet <ellet@freshplatform.net>
2024-02-17 02:56:51 +0300

    Update the example

--- example/lib/screens/settings/cubit/settings_cubit.freezed.dart
+++ example/lib/screens/settings/cubit/settings_cubit.freezed.dart
@@ -12,7 +12,7 @@ part of 'settings_cubit.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 SettingsState _$SettingsStateFromJson(Map<String, dynamic> json) {
   return _SettingsState.fromJson(json);
