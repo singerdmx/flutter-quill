ea7ba7e712e6e322472189ab63e16967c7b7d62a
Douglas Ward <dward@scied.com>
2024-04-01 17:48:50 -0600

    color_button calls afterButtonPressed insert at start of line uses style for line

--- lib/src/models/rules/insert.dart
+++ lib/src/models/rules/insert.dart
@@ -559,10 +559,16 @@ class PreserveInlineStylesRule extends InsertRule {
     }
 
     final itr = DeltaIterator(document);
-    final prev = itr.skip(len == 0 ? index : index + 1);
+    var prev = itr.skip(len == 0 ? index : index + 1);
+    //
+    //  Inserting at start of line should use style for first character on the line
+    //
     if (prev == null ||
-        prev.data is! String ||
-        (prev.data as String).contains('\n')) {
+        (prev.data is String && (prev.data as String).endsWith('\n'))) {
+      prev = itr.next();
+    }
+
+    if (prev.data is! String) {
       return null;
     }
 
