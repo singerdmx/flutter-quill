00783adf915a07209220f3c8c84b7695b56c52bd
AtlasAutocode <AtlasAutocode@gmail.com>
2024-06-07 15:20:06 -0600

    Subscript and Superscript

--- lib/src/models/config/quill_controller_configurations.dart
+++ lib/src/models/config/quill_controller_configurations.dart
@@ -1,8 +1,14 @@
 class QuillControllerConfigurations {
-  const QuillControllerConfigurations({this.onClipboardPaste});
+  const QuillControllerConfigurations(
+      {this.onClipboardPaste, this.requireScriptFontFeatures = false});
 
   /// Callback when the user pastes and data has not already been processed
   ///
   /// Return true if the paste operation was handled
   final Future<bool> Function()? onClipboardPaste;
+
+  /// Render subscript and superscript text using Open Type FontFeatures
+  ///
+  /// Default is false to use built-in script rendering that is independent of font capabilities
+  final bool requireScriptFontFeatures;
 }
