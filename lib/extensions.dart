bac235169f3bd3d83fe51c941ea5c97f8f85eaa0
Guillermo Bueno Vargas <Guillergood@users.noreply.github.com>
2024-07-19 17:35:40 +0200

    Have the ability to create custom rules, closes #1162

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -4,4 +4,7 @@ export 'src/common/utils/platform.dart';
 export 'src/common/utils/string.dart';
 export 'src/common/utils/widgets.dart';
 export 'src/document/nodes/leaf.dart';
+export 'src/rules/rule.dart';
 export 'src/rules/insert.dart';
+export 'src/rules/format.dart';
+export 'src/rules/delete.dart';
