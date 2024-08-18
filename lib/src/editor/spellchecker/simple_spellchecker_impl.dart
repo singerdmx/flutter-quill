b8ff7902bab08d3d5e1f39f00efa788ffc3d54ff
CatHood0 <santiagowmar@gmail.com>
2024-08-18 06:44:25 -0400

    Doc: more documentation about the new service

--- lib/src/editor/spellchecker/simple_spellchecker_impl.dart
+++ lib/src/editor/spellchecker/simple_spellchecker_impl.dart
@@ -4,12 +4,17 @@ import 'package:simple_spell_checker/simple_spell_checker.dart';
 
 import 'spellchecker_service.dart';
 
+/// SimpleSpellCheckerImpl is a simple spell checker for get 
+/// all words divide on different objects if them are wrong or not
 class SimpleSpellCheckerImpl extends SpellcheckerService {
   SimpleSpellCheckerImpl({required super.language})
       : checker = SimpleSpellChecker(
           language: language,
           safeDictionaryLoad: true,
         );
+  /// [SimpleSpellChecker] comes from the package [simple_spell_checker]
+  /// that give us all necessary methods for get our spans with highlighting
+  /// where needed 
   final SimpleSpellChecker checker;
 
   @override
@@ -26,7 +31,11 @@ class SimpleSpellCheckerImpl extends SpellcheckerService {
   }
 
   @override
-  void dispose() {
+  void dispose({bool onlyPartial = false}) {
+    if(onlyPartial) {
+      checker.disposeControllers();
+      return;
+    }
     checker.dispose(closeDirectionary: true);
   }
 }
--- lib/src/editor/spellchecker/spellchecker_service.dart
+++ lib/src/editor/spellchecker/spellchecker_service.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+/// A representation a custom SpellCheckService.
 abstract class SpellcheckerService {
   SpellcheckerService({required this.language});
 
   final String language;
+  /// dispose all the resources used for SpellcheckerService
+  /// 
+  /// if [onlyPartial] is true just dispose a part of the SpellcheckerService 
+  /// (this comes from the implementation)
+  ///
+  /// if [onlyPartial] is false dispose all resources
+  void dispose({bool onlyPartial = false});
 
-  void dispose();
+  /// Facilitates a spell check request.
+  ///
+  /// Returns a [List<TextSpan>] with all misspelled words divide from the right words.
   List<TextSpan>? fetchSpellchecker(String text,
       {LongPressGestureRecognizer Function(String)?
           customLongPressRecognizerOnWrongSpan});
