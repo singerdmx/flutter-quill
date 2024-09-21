708baf2ed4138e809aebe70b3ab0ae03a8ccd914
Ellet <echo.ellet@gmail.com>
2024-09-21 05:09:57 +0300

    chore: deprecate spell checker service (#2255)
    * chore: deprecate SpellCheckerService
    
    * docs(readme): update docs to reflect the change
    
    * chore: deprecate DefaultSpellCheckerService and SpellCheckerServiceProvider, ignore all warnings

--- lib/src/editor/spellchecker/default_spellchecker_service.dart
+++ lib/src/editor/spellchecker/default_spellchecker_service.dart
@@ -1,10 +1,19 @@
+// ignore_for_file: deprecated_member_use_from_same_package
+
 import 'package:flutter/gestures.dart' show LongPressGestureRecognizer;
 import 'package:flutter/material.dart' show TextSpan;
+import 'package:meta/meta.dart';
 import 'spellchecker_service.dart' show SpellCheckerService;
 
 /// A default implementation of the [SpellcheckerService]
 /// that always will return null since Spell checking
 /// is not a standard feature
+@Deprecated(
+  'A breaking change is being planned for the SpellCheckerService and SpellCheckerServiceProvider.\n'
+  "A replacement doesn't exist yet but should arrive soon."
+  'See https://github.com/singerdmx/flutter-quill/issues/2246 for more details.',
+)
+@experimental
 class DefaultSpellCheckerService extends SpellCheckerService<Object?> {
   DefaultSpellCheckerService() : super(language: 'en');
 
