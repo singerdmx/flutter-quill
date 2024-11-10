5c681cd2b13e641bb736bdb446077b7fe77e5b63
Ellet <echo.ellet@gmail.com>
2024-11-10 16:03:06 +0300

    chore: check the pubspec.yaml version after updating the version in the script

--- scripts/pubspec_version_check.dart
+++ scripts/pubspec_version_check.dart
@@ -4,45 +4,33 @@ import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-const _usage =
-    'Usage: ./pubspec_version_check.dart <version> <pubspec-file-path>';
-
 /// Validate a version to match with the version in pubspec.yaml file
-void main(List<String> args) {
-  print('The passed args: $args');
-  if (args.isEmpty) {
-    print('Missing required arguments. $_usage');
+void checkPubspecVersion({
+  required String expectedVersion,
+  required String pubspecFilePath,
+}) {
+  if (expectedVersion.isEmpty) {
+    print('The version is empty.');
     exit(1);
   }
-  if (args.length > 2) {
-    print('Too many arguments. $_usage');
+
+  if (pubspecFilePath.isEmpty) {
+    print('The pubspec file path is empty.');
     exit(1);
   }
-  if (args.length != 2) {
-    print('Should only pass 2 arguments. $_usage');
-    exit(1);
-  }
-  final version = args[0];
-  if (version.isEmpty) {
-    print('The version is empty. $_usage');
-    exit(1);
-  }
-  final pubspecPath = args[1];
-  if (pubspecPath.isEmpty) {
-    print('The pubspec file path is empty. $_usage');
-    exit(1);
-  }
-  final pubspecFile = File(pubspecPath);
+  final pubspecFile = File(pubspecFilePath);
   if (!pubspecFile.existsSync()) {
     print('The pubspec file does not exist: ${pubspecFile.absolute.path}');
     exit(1);
   }
   final pubspecYaml = loadYaml(pubspecFile.readAsStringSync());
   final pubspecVersion = pubspecYaml['version'];
-  if (version != pubspecVersion) {
+  if (expectedVersion != pubspecVersion) {
     print(
-        'The version ($version) does not match the version in pubspec.yaml ($pubspecVersion).');
+      'The version ($expectedVersion) does not match the version in pubspec.yaml ($pubspecVersion).\n'
+      'The pubspec.yaml file is located at: ${pubspecFile.absolute.path}',
+    );
     exit(1);
   }
-  print('The version ($version) match the version in pubspec.yaml');
+  print('The version ($expectedVersion) match the version in pubspec.yaml');
 }
