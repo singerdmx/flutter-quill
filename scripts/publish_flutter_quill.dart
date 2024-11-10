b27b89c269038d178eaed935d569c527dfe7c31f
Ellet <echo.ellet@gmail.com>
2024-11-10 17:20:51 +0300

    chore: minor changes to the publish_flutter_quill.dart script

--- scripts/publish_flutter_quill.dart
+++ scripts/publish_flutter_quill.dart
@@ -18,38 +18,47 @@ const _targetChangelog = './CHANGELOG.md';
 
 const _confirmPublishOptionName = 'Y';
 
+const _changelogAndPubspecRestoreMessage =
+    'ℹ️ Changes to CHANGELOG.md and pubspec.yaml have not been reverted.\n'
+    'To revert them, run:\n'
+    'git restore --staged $_targetChangelog $_targetPubspecYaml\n'
+    'git restore $_targetChangelog $_targetPubspecYaml';
+
 void main(List<String> args) {
-  print('The passed args: $args');
+  print('➡️ Arguments provided: $args');
+
   if (args.isEmpty) {
-    print('Missing required arguments. $_usage');
+    print('❌ Missing required arguments. $_usage');
     exit(1);
   }
   if (args.length > 1) {
-    print('Too many arguments. $_usage');
+    print('❌ Too many arguments. $_usage');
     exit(1);
   }
   if (args.length != 1) {
-    print('Expect one argument. $_usage');
+    print('❌ Expected a single argument. $_usage');
     exit(1);
   }
   final version = args[0];
   if (version.isEmpty) {
-    print('The version is empty. $_usage');
+    print('❌ The version is empty. $_usage');
     exit(1);
   }
   if (version.startsWith('v')) {
-    print('The version ($version) should not start with v.');
+    print(
+      '❌ Version ($version) should not start with `v`, as the script will add it to the tag.',
+    );
     exit(1);
   }
   final confirmPublish =
       args.elementAtOrNull(3) == '-$_confirmPublishOptionName';
-  if (!isValidVersion(version)) {
-    print('The version ($version) is invalid.');
+  if (!_isValidVersion(version)) {
+    print('❌ Invalid version format ($version).');
     exit(1);
   }
-  if (!isGitClean()) {
+  if (!_isGitClean()) {
     print(
-        'The Git working tree is not clean. Commit your changes and run again.');
+        '❌ Git working directory is not clean. Commit all changes and try again.');
     exit(1);
   }
   updatePubspecVersion(
@@ -65,53 +74,65 @@ void main(List<String> args) {
     changelogFilePath: _targetChangelog,
   );
 
-  Process.runSync('git', ['add', _targetPubspecYaml, _targetChangelog]);
-  if (isGitClean()) {
-    print(
-      'The Git working tree is still clean after updating $_targetChangelog and $_targetChangelog. Fix the script before continuing.',
-    );
-    exit(1);
-  }
+  try {
+    Process.runSync('git', ['add', _targetPubspecYaml, _targetChangelog]);
 
-  print('✅ CHANGELOG and pubspec.yaml files has been updated.');
-  if (!confirmPublish) {
-    print(
-      'To confirm the publish, type `$_confirmPublishOptionName` and then confirm the input.\n'
-      'FYI: You can add `-$_confirmPublishOptionName` when running the script to skip this check.',
-    );
-    final confirm = stdin.readLineSync();
-    if (confirm != _confirmPublishOptionName) {
+    if (_isGitClean()) {
       print(
-        'The publishing process has been aborted.\n'
-        'Changes to CHANGELOG.md and pubspec.yaml have not been reverted.\n'
-        'To revert them, run:\n'
-        'git restore --staged $_targetChangelog $_targetPubspecYaml\n'
-        'git restore $_targetChangelog $_targetPubspecYaml',
+        '❌ No changes detected after updating $_targetChangelog and $_targetPubspecYaml.\n'
+        'Review the script for potential issues.',
       );
       exit(1);
     }
+
+    print('✅ CHANGELOG and pubspec.yaml files have been updated.');
+    if (!confirmPublish) {
+      print(
+        'ℹ️ To confirm publishing, type `$_confirmPublishOptionName` and press Enter.\n'
+        'Tip: Add `-$_confirmPublishOptionName` as an argument to skip this prompt in the future.',
+      );
+      final confirm = stdin.readLineSync();
+      if (confirm != _confirmPublishOptionName) {
+        print('❌ The publishing process has been aborted.\n'
+            '$_changelogAndPubspecRestoreMessage');
+        exit(1);
+      }
+    }
+
+    final tagName = 'v$version';
+
+    print('ℹ️ Committing changes...');
+    Process.runSync(
+        'git', ['commit', '-m', 'chore(release): prepare to publish $version']);
+
+    print('ℹ️ Creating git tag `$tagName`...');
+    Process.runSync('git', ['tag', tagName]);
+
+    print('ℹ️ Pushing commit to remote...');
+    Process.runSync('git', ['push']);
+
+    print('ℹ️ Pushing tag to remote...');
+    Process.runSync('git', ['push', 'origin', tagName]);
+    print(
+      '✅ The tag $tagName has been pushed. The GitHub workflow will handle the rest.\n'
+      'For more details, check: https://github.com/singerdmx/flutter-quill/actions',
+    );
+  } catch (e) {
+    print(
+      '❌ An error occurred during the publishing process: ${e.toString()}\n\n',
+    );
+    print(_changelogAndPubspecRestoreMessage);
   }
-
-  final tagName = 'v$version';
-  Process.runSync(
-      'git', ['commit', '-m', 'chore(release): prepare to publish $version']);
-  Process.runSync('git', ['tag', tagName]);
-
-  Process.runSync('git', ['push']);
-  Process.runSync('git', ['push', 'origin', tagName]);
-  print(
-    '✅ The tag $tagName has been pushed, the GitHub workflow will do the rest.',
-  );
 }
 
 // Same as https://dart.dev/tools/pub/automated-publishing#configuring-a-github-action-workflow-for-publishing-to-pub-dev
 // but without the `v`.
-bool isValidVersion(String version) {
+bool _isValidVersion(String version) {
   final regex = RegExp(r'^[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+(\.[0-9]+)?)?$');
   return regex.hasMatch(version);
 }
 
-bool isGitClean() {
+bool _isGitClean() {
   final result = Process.runSync('git', ['status', '--porcelain']);
   // If the output is empty, the repository is clean
   return result.stdout.toString().trim().isEmpty;
