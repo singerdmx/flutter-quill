aaa969ed16ae9ec391543b0dcc5e597348093996
CatHood0 <santiagowmar@gmail.com>
2025-03-13 13:27:05 -0400

    Revert: additions to test file of flutter_quill_test

--- flutter_quill_test/test/flutter_quill_test_test.dart
+++ flutter_quill_test/test/flutter_quill_test_test.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_quill_test/flutter_quill_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('placeholder test', (tester) async {
-    await tester.quillUpdateEditingValueWithSelection(find.text(''));
-  });
+  test('placeholder test', () {});
 }
