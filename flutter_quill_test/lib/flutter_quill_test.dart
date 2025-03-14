5b52f375e733d6734c6a9c553906522c46c863d3
CatHood0 <santiagowmar@gmail.com>
2025-03-14 00:26:48 -0400

    Chore: added toolbar and selection nodes interactions

--- flutter_quill_test/lib/flutter_quill_test.dart
+++ flutter_quill_test/lib/flutter_quill_test.dart
@@ -1,6 +1,8 @@
 library;
 
 export 'src/test/selection_interactions/widget_tester_cursor_movement_extension.dart';
+export 'src/test/selection_interactions/widget_tester_nodes_selection_extension.dart';
+export 'src/test/selection_interactions/widget_tester_toolbar_extension.dart';
 export 'src/test/text_interactions/widget_tester_insert_extension.dart';
 export 'src/test/text_interactions/widget_tester_remove_extension.dart';
 export 'src/test/text_interactions/widget_tester_replace_extension.dart';
--- flutter_quill_test/test/flutter_quill_test_test.dart
+++ flutter_quill_test/test/flutter_quill_test_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill_test/flutter_quill_test.dart';
+import 'package:flutter_quill_test/src/test/selection_interactions/widget_tester_toolbar_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -59,5 +60,68 @@ void main() {
     // Remove the selected text
     await tester.quillRemoveTextInSelection(find.byType(QuillEditor));
     expect(controller.document.toPlainText(), 'World and hi, World!\n');
+  });
+
+  testWidgets('Test QuillEditor toolbar interactions', (tester) async {
+    // Build the QuillEditor widget
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: <LocalizationsDelegate>[
+          FlutterQuillLocalizations.delegate,
+        ],
+        home: Column(
+          children: [
+            QuillSimpleToolbar(
+              controller: controller,
+              config: const QuillSimpleToolbarConfig(
+                headerStyleType: HeaderStyleType.buttons,
+                multiRowsDisplay: false,
+                showAlignmentButtons: true,
+                showLineHeightButton: true,
+              ),
+            ),
+            QuillEditor.basic(
+              controller: controller,
+            ),
+          ],
+        ),
+      ),
+    );
+    await tester.idle();
+
+    await tester.tap(find.byType(QuillEditor));
+    // Enter text
+    await tester.quillEnterText(find.byType(QuillEditor), 'Hello, World!\n');
+    await tester.idle();
+    expect(controller.document.toPlainText(), 'Hello, World!\n');
+    expect(controller.selection.isCollapsed, isTrue);
+
+    await tester.quillUpdateSelection(find.byType(QuillEditor), 0, 5);
+    expect(controller.selection.isCollapsed, false);
+    expect(controller.selection,
+        const TextSelection(baseOffset: 0, extentOffset: 5));
+
+    var nodes = List<Node>.from(
+      await tester.getNodesInSelection(
+        find.byType(QuillEditor),
+      ),
+    );
+    expect((nodes.first as Line).first?.toPlainText(), 'Hello, World!');
+    expect((nodes.first as Line).first?.style.toJson(), isNull);
+
+    await tester.pressHeaderToolbarOption(
+      find.byType(QuillEditor),
+      2,
+    );
+
+    nodes = List<Node>.from(
+      await tester.getNodesInSelection(
+        find.byType(QuillEditor),
+      ),
+    );
+    expect((nodes.first as Line).first?.toPlainText(), 'Hello');
+    expect((nodes.first as Line).first?.style.toJson(), {'bold': true});
+    expect((nodes.first as Line).last.toPlainText(), ', World!');
+    expect((nodes.first as Line).last.style.toJson(), isNull);
   });
 }
