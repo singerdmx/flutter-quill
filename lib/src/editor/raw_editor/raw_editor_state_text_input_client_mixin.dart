4188c3420b17fbf1bb49104b2f3f3446c93f4815
Ellet <echo.ellet@gmail.com>
2025-03-23 22:03:04 +0300

    fix: QuillEditor doesn't respect the system keyboard brightness by default on iOS (#2522)
    * fix: QuillEditor doesn't respect the system keyboard brightness by default on iOS
    
    * test: add tests for keyboardAppearance, characterShortcutEvents and spaceShortcutEvents are no longer required, Deprecate  in favor of

--- lib/src/editor/raw_editor/raw_editor_state_text_input_client_mixin.dart
+++ lib/src/editor/raw_editor/raw_editor_state_text_input_client_mixin.dart
@@ -1,11 +1,11 @@
 import 'dart:ui' show lerpDouble;
 
-import 'package:flutter/animation.dart' show Curves;
-import 'package:flutter/cupertino.dart' show CupertinoTheme;
-import 'package:flutter/foundation.dart' show ValueNotifier, kIsWeb;
-import 'package:flutter/material.dart' show Theme;
-import 'package:flutter/scheduler.dart' show SchedulerBinding;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 
 import '../../delta/delta_diff.dart';
 import '../../document/document.dart';
@@ -62,6 +62,14 @@ mixin RawEditorStateTextInputClientMixin on EditorState
     }
   }
 
+  /// This setting is only honored on iOS devices.
+  @visibleForTesting
+  @internal
+  Brightness createKeyboardAppearance() =>
+      widget.config.keyboardAppearance ??
+      CupertinoTheme.maybeBrightnessOf(context) ??
+      Theme.of(context).brightness;
+
   void openConnectionIfNeeded() {
     if (!shouldCreateInputConnection) {
       return;
@@ -76,9 +84,7 @@ mixin RawEditorStateTextInputClientMixin on EditorState
           readOnly: widget.config.readOnly,
           inputAction: widget.config.textInputAction,
           enableSuggestions: !widget.config.readOnly,
-          keyboardAppearance: widget.config.keyboardAppearance ??
-              CupertinoTheme.maybeBrightnessOf(context) ??
-              Theme.of(context).brightness,
+          keyboardAppearance: createKeyboardAppearance(),
           textCapitalization: widget.config.textCapitalization,
           allowedMimeTypes: widget.config.contentInsertionConfiguration == null
               ? const <String>[]
