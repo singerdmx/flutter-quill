b5389a967b62fd3a112c9a768d05bbc36c538756
SharipovShohruh1106 <shohruhmustang@gmail.com>
2025-03-24 15:49:35 +0500

    added uzbek language

---
+++ lib/src/l10n/generated/quill_localizations_uz.dart
@@ -0,0 +1,293 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'quill_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Turkish (`tr`).
+class FlutterQuillLocalizationsUz extends FlutterQuillLocalizations {
+  FlutterQuillLocalizationsUz([String locale = 'uz']) : super(locale);
+
+  @override
+  String get pasteLink => 'Havolani qo‘yish';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get selectColor => 'Rangni tanlash';
+
+  @override
+  String get gallery => 'Galereya';
+
+  @override
+  String get link => 'Havola';
+
+  @override
+  String get open => 'Ochish';
+
+  @override
+  String get copy => 'Nusxa olish';
+
+  @override
+  String get remove => 'Oʻchirish';
+
+  @override
+  String get save => 'Saqlash';
+
+  @override
+  String get zoom => 'Kattalashtirish';
+
+  @override
+  String get saved => 'Saqlandi';
+
+  @override
+  String get text => 'Matn';
+
+  @override
+  String get resize => 'O‘lchamini o‘zgartirish';
+
+  @override
+  String get width => 'Kenglik';
+
+  @override
+  String get height => 'Balandlik';
+
+  @override
+  String get size => 'O‘lcham';
+
+  @override
+  String get small => 'Kichik';
+
+  @override
+  String get large => 'Katta';
+
+  @override
+  String get huge => 'Juda katta';
+
+  @override
+  String get clear => 'Tozalash';
+
+  @override
+  String get font => 'Shrift';
+
+  @override
+  String get search => 'Qidirish';
+
+  @override
+  String get camera => 'Kamera';
+
+  @override
+  String get video => 'Video';
+
+  @override
+  String get undo => 'Bekor qilish';
+
+  @override
+  String get redo => 'Qayta bajarish';
+
+  @override
+  String get fontFamily => 'Shrift oilasi';
+
+  @override
+  String get fontSize => 'Shrift oʻlchami';
+
+  @override
+  String get bold => 'Qalin';
+
+  @override
+  String get italic => 'Yotiq';
+
+  @override
+  String get underline => 'Tagchiziq';
+
+  @override
+  String get strikeThrough => 'Ustchiziq';
+
+  @override
+  String get inlineCode => 'Ichki kod';
+
+  @override
+  String get fontColor => 'Shrift rangi';
+
+  @override
+  String get backgroundColor => 'Fon rangi';
+
+  @override
+  String get clearFormat => 'Formatni tozalash';
+
+  @override
+  String get alignLeft => 'Chapga tekislash';
+
+  @override
+  String get alignCenter => 'Markazga tekislash';
+
+  @override
+  String get alignRight => 'O‘ngga tekislash';
+
+  @override
+  String get justifyWinWidth => 'Oynaning kengligiga tekislash';
+
+  @override
+  String get textDirection => 'Matn yo‘nalishi';
+  @override
+  String get headerStyle => 'Sarlavha uslubi';
+
+  @override
+  String get numberedList => 'Raqamlangan ro‘yxat';
+
+  @override
+  String get bulletList => 'Belgilangan ro‘yxat';
+  @override
+  String get checkedList => 'Tekshirish belgili ro\'yxat';
+  @override
+  String get codeBlock => 'Kod bloki';
+  @override
+  String get quote => 'Iqtibos';
+  @override
+  String get increaseIndent => 'Ichkariga surish';
+  @override
+  String get decreaseIndent => 'Tashqariga surish';
+  @override
+  String get insertURL => 'URL qo‘shish';
+  @override
+  String get visitLink => 'Havolaga o‘tish';
+  @override
+  String get enterLink => 'Havola kiritish';
+  @override
+  String get enterMedia => 'Media kiritish';
+  @override
+  String get edit => 'Tahrirlash';
+  @override
+  String get apply => 'Qo‘llash';
+  @override
+  String get findText => 'Matnni topish';
+  @override
+  String get moveToPreviousOccurrence => 'Oldingi takroriy qismga o‘tish';
+  @override
+  String get moveToNextOccurrence => 'Keyingi takroriy qismga o‘tish';
+  @override
+  String get savedUsingTheNetwork => 'Tarmoq orqali saqlandi';
+  @override
+  String get savedUsingLocalStorage => 'Mahalliy xotirada saqlandi';
+  @override
+  String get errorWhileSavingImage => 'Rasmni saqlashda xatolik yuz berdi';
+  @override
+  String get pleaseEnterTextForYourLink => "masalan, 'Batafsil ma’lumot'";
+  @override
+  String get pleaseEnterTheLinkURL => "masalan, 'https://example.com'";
+  @override
+  String get pleaseEnterAValidImageURL =>
+      'Iltimos, yaroqli rasm URL manzilini kiriting';
+  @override
+  String get hex => 'Hex';
+  @override
+  String get material => 'Material';
+  @override
+  String get color => 'Rang';
+  @override
+  String get lineheight => 'Chiziq balandligi';
+  @override
+  String get pleaseEnterAValidVideoURL =>
+      'Iltimos, yaroqli video URL manzilini kiriting';
+  @override
+  String get photo => 'Foto';
+  @override
+  String get image => 'Rasm';
+  @override
+  String get caseSensitivityAndWholeWordSearch =>
+      'Katta-kichik harflarga sezgirlik va to‘liq so‘z bo‘yicha qidirish';
+  @override
+  String get insertImage => 'Rasm qo‘shish';
+
+  @override
+  String get pickAPhotoFromYourGallery => 'Video qo‘shish';
+
+  @override
+  String get alignJustify => 'To‘liq tekislash';
+
+  @override
+  String get takeAPhotoUsingYourCamera => 'Kamera orqali suratga olish';
+
+  @override
+  String get pasteAPhotoUsingALink => 'Havola orqali rasm joylashtirish';
+
+  @override
+  String get pickAVideoFromYourGallery => 'Galereyadan video tanlang';
+
+  @override
+  String get recordAVideoUsingYourCamera => 'Kamera orqali videoga oling';
+  @override
+  String get normal => 'Oddiy';
+  @override
+  String get heading1 => 'Sarlavha 1';
+  @override
+  String get heading2 => 'Sarlavha 2';
+  @override
+  String get heading3 => 'Sarlavha 3';
+  @override
+  String get heading4 => 'Sarlavha 4';
+  @override
+  String get heading5 => 'Sarlavha 5';
+  @override
+  String get heading6 => 'Sarlavha 6';
+  @override
+  String theImageHasBeenSavedAt(String imagePath) =>
+      'Rasm shu manzilda saqlandi: $imagePath';
+  @override
+  String get caseSensitive => 'Katta-kichik harflarga sezuvchan';
+  @override
+  String get wholeWord => 'To‘liq so‘z';
+
+  @override
+  String get pasteAVideoUsingALink => 'Havola orqali video qo‘shish';
+
+  @override
+  String get close => 'Yopish';
+  @override
+  String get searchSettings => 'Qidiruv sozlamalari';
+
+  @override
+  String get cut => 'Kesish';
+
+  @override
+  String get paste => 'Joylashtirish';
+
+  @override
+  String get insertTable => 'Jadval qo‘shish';
+
+  @override
+  String get subscript => 'Pastki indeks';
+
+  @override
+  String get superscript => 'Yuqori indeks';
+
+  @override
+  String get insertVideo => 'Video qo‘shish';
+
+  @override
+  String get errorUnexpectedSavingImage =>
+      "Rasmni saqlashda kutilmagan xatolik yuz berdi. Iltimos, qayta urinib ko'ring.";
+
+  @override
+  String get successImageSavedGallery => 'Rasm galereyaga saqlandi.';
+
+  @override
+  String get successImageSaved => 'Rasm muvaffaqiyatli saqlandi.';
+
+  @override
+  String get successImageDownloaded => 'Rasm muvaffaqiyatli yuklab olindi.';
+
+  @override
+  String get openGallery => 'Galereyani ochish';
+
+  @override
+  String get openFileLocation => 'Fayl joylashuvini ochish';
+
+  @override
+  String get openFile => 'Faylni ochish';
+
+  @override
+  String get saveImagePermissionDenied =>
+      'Rasmni saqlash uchun ruxsat yetarli emas';
+}
