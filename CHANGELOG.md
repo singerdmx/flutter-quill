da4be28ddab40522d3518584ccee07b4dc0d9470
RIfat Khan <102645154+md-rifatkhan@users.noreply.github.com>
2025-04-27 15:42:29 +0600

    Add copyWith methods to DefaultStyles, HorizontalSpacing, and VerticalSpacing (#2550)
    * feat: add copyWith
    
    * docs(changelog): add changes
    
    ---------
    
    Co-authored-by: Ellet <echo.ellet@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- `copyWith` methods to `HorizontalSpacing`, `VerticalSpacing`, `DefaultTextBlockStyle`, and `DefaultListBlockStyle` for immutable updates of properties [#2550](https://github.com/singerdmx/flutter-quill/pull/2550).
+
 ## [11.4.0] - 2025-04-23
 
 ### Added
--- lib/src/common/structs/horizontal_spacing.dart
+++ lib/src/common/structs/horizontal_spacing.dart
@@ -11,4 +11,24 @@ class HorizontalSpacing {
   final double right;
 
   static const zero = HorizontalSpacing(0, 0);
+
+  HorizontalSpacing copyWith({
+    double? left,
+    double? right,
+  }) {
+    return HorizontalSpacing(
+      left ?? this.left,
+      right ?? this.right,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is HorizontalSpacing &&
+              left == other.left &&
+              right == other.right;
+
+  @override
+  int get hashCode => Object.hash(left, right);
 }
