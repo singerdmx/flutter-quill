e0dea6eecde9a53ac778718469d4a2eb929ec0b4
Tristan Clarke <tristanclarke1993@gmail.com>
2025-05-14 11:37:09 +0100

    Generate files

---
+++ lib/src/l10n/generated/quill_localizations_fi.dart
@@ -0,0 +1,336 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'quill_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Finnish (`fi`).
+class FlutterQuillLocalizationsFi extends FlutterQuillLocalizations {
+  FlutterQuillLocalizationsFi([String locale = 'fi']) : super(locale);
+
+  @override
+  String get pasteLink => 'Liitä linkki';
+
+  @override
+  String get ok => 'Ok';
+
+  @override
+  String get selectColor => 'Valitse väri';
+
+  @override
+  String get gallery => 'Galleria';
+
+  @override
+  String get link => 'Linkki';
+
+  @override
+  String get open => 'Avaa';
+
+  @override
+  String get copy => 'Kopioi';
+
+  @override
+  String get remove => 'Poista';
+
+  @override
+  String get save => 'Tallenna';
+
+  @override
+  String get zoom => 'Zoomaa';
+
+  @override
+  String get saved => 'Tallennettu';
+
+  @override
+  String get text => 'Teksti';
+
+  @override
+  String get resize => 'Kokoa';
+
+  @override
+  String get width => 'Leveys';
+
+  @override
+  String get height => 'Korkeus';
+
+  @override
+  String get size => 'Koko';
+
+  @override
+  String get small => 'Pieni';
+
+  @override
+  String get large => 'Iso';
+
+  @override
+  String get huge => 'Hirmuinen';
+
+  @override
+  String get clear => 'Tyhjennä';
+
+  @override
+  String get font => 'Fontti';
+
+  @override
+  String get search => 'Etsi';
+
+  @override
+  String get camera => 'Kamera';
+
+  @override
+  String get video => 'Video';
+
+  @override
+  String get undo => 'Kumoa';
+
+  @override
+  String get redo => 'Tee uudelleen';
+
+  @override
+  String get fontFamily => 'Fonttiperhe';
+
+  @override
+  String get fontSize => 'Fonttikoko';
+
+  @override
+  String get bold => 'Lihavointi';
+
+  @override
+  String get subscript => 'Alaviite';
+
+  @override
+  String get superscript => 'Yliviite';
+
+  @override
+  String get italic => 'Kursiivi';
+
+  @override
+  String get underline => 'Alleviivaus';
+
+  @override
+  String get strikeThrough => 'Yliviivaus';
+
+  @override
+  String get inlineCode => 'Riviin upotettu koodi';
+
+  @override
+  String get fontColor => 'Fontin väri';
+
+  @override
+  String get backgroundColor => 'Taustaväri';
+
+  @override
+  String get clearFormat => 'Poista muotoilu';
+
+  @override
+  String get alignLeft => 'Tasaa vasemmalle';
+
+  @override
+  String get alignCenter => 'Tasaa keskelle';
+
+  @override
+  String get alignRight => 'Tasaa oikealle';
+
+  @override
+  String get alignJustify => 'Tasaa molemmille reunoille';
+
+  @override
+  String get justifyWinWidth => 'Tasaa ikkunan leveys';
+
+  @override
+  String get textDirection => 'Tekstin suunta';
+
+  @override
+  String get headerStyle => 'Otsikkotyyli';
+
+  @override
+  String get normal => 'Normaali';
+
+  @override
+  String get heading1 => 'Otsikko 1';
+
+  @override
+  String get heading2 => 'Otsikko 2';
+
+  @override
+  String get heading3 => 'Otsikko 3';
+
+  @override
+  String get heading4 => 'Otsikko 4';
+
+  @override
+  String get heading5 => 'Otsikko 5';
+
+  @override
+  String get heading6 => 'Otsikko 6';
+
+  @override
+  String get numberedList => 'Numeroitu lista';
+
+  @override
+  String get bulletList => 'Ruutumerkki lista';
+
+  @override
+  String get checkedList => 'Rasti-lista';
+
+  @override
+  String get codeBlock => 'Koodilohko';
+
+  @override
+  String get quote => 'Lainaus';
+
+  @override
+  String get increaseIndent => 'Lisää sisennys';
+
+  @override
+  String get decreaseIndent => 'Vähennä sisennys';
+
+  @override
+  String get insertURL => 'Lisää URL';
+
+  @override
+  String get visitLink => 'Vieraile linkissä';
+
+  @override
+  String get enterLink => 'Syötä linkki';
+
+  @override
+  String get enterMedia => 'Syötä media';
+
+  @override
+  String get edit => 'Muokkaa';
+
+  @override
+  String get apply => 'Käytä';
+
+  @override
+  String get hex => 'Hex';
+
+  @override
+  String get material => 'Materiaali';
+
+  @override
+  String get color => 'Väri';
+
+  @override
+  String get lineheight => 'Riviväli';
+
+  @override
+  String get findText => 'Etsi teksti';
+
+  @override
+  String get moveToPreviousOccurrence => 'Siirry edelliseen esiintymään';
+
+  @override
+  String get moveToNextOccurrence => 'Siirry seuraavaan esiintymään';
+
+  @override
+  String get savedUsingTheNetwork => 'Tallennettu verkon kautta';
+
+  @override
+  String get savedUsingLocalStorage =>
+      'Tallennettu paikalliseen tallennustilaan';
+
+  @override
+  String theImageHasBeenSavedAt(String imagePath) {
+    return 'Kuva on tallennettu osoitteeseen: $imagePath';
+  }
+
+  @override
+  String get errorWhileSavingImage => 'Virhe kuvan tallentamisessa';
+
+  @override
+  String get pleaseEnterTextForYourLink =>
+      'Anna linkkisi teksti (esim. \'Lue lisää\')';
+
+  @override
+  String get pleaseEnterTheLinkURL =>
+      'Anna linkin URL (esim. \'https://example.com\')';
+
+  @override
+  String get pleaseEnterAValidImageURL => 'Anna kelvollinen kuvan URL';
+
+  @override
+  String get pleaseEnterAValidVideoURL => 'Anna kelvollinen videon URL';
+
+  @override
+  String get photo => 'Valokuva';
+
+  @override
+  String get image => 'Kuva';
+
+  @override
+  String get caseSensitivityAndWholeWordSearch =>
+      'Harkitse isot ja pienet kirjaimet sekä koko sanan haku';
+
+  @override
+  String get caseSensitive => 'Isot ja pienet kirjaimet huomioiden';
+
+  @override
+  String get wholeWord => 'Koko sana';
+
+  @override
+  String get insertImage => 'Lisää kuva';
+
+  @override
+  String get pickAPhotoFromYourGallery => 'Valitse valokuva galleriastasi';
+
+  @override
+  String get takeAPhotoUsingYourCamera => 'Ota valokuva kamerallasi';
+
+  @override
+  String get pasteAPhotoUsingALink => 'Liitä valokuva linkillä';
+
+  @override
+  String get pickAVideoFromYourGallery => 'Valitse video galleriastasi';
+
+  @override
+  String get recordAVideoUsingYourCamera => 'Tallenna video kamerallasi';
+
+  @override
+  String get pasteAVideoUsingALink => 'Liitä video linkillä';
+
+  @override
+  String get close => 'Sulje';
+
+  @override
+  String get searchSettings => 'Etsi asetukset';
+
+  @override
+  String get cut => 'Leikkaa';
+
+  @override
+  String get paste => 'Liitä';
+
+  @override
+  String get insertTable => 'Lisää taulukko';
+
+  @override
+  String get insertVideo => 'Lisää video';
+
+  @override
+  String get errorUnexpectedSavingImage =>
+      'Odottamaton virhe kuvan tallentamisessa. Yritä uudelleen.';
+
+  @override
+  String get successImageSavedGallery => 'Kuva tallennettu galleriaasi.';
+
+  @override
+  String get successImageSaved => 'Kuva tallennettu onnistuneesti.';
+
+  @override
+  String get successImageDownloaded => 'Kuva ladattu onnistuneesti.';
+
+  @override
+  String get openGallery => 'Avaa galleria';
+
+  @override
+  String get openFileLocation => 'Avaa tiedoston sijainti';
+
+  @override
+  String get openFile => 'Avaa tiedosto';
+
+  @override
+  String get saveImagePermissionDenied =>
+      'Kuvan tallentaminen estetty puuttuvan luvan takia';
+}
