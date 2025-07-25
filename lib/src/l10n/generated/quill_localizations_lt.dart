0e1f795acd69bbd072af9d6cf8df7e1c30d7b559
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2025-07-25 06:46:21 +0300

    feat: add Lithuanian translations for FlutterQuill

---
+++ lib/src/l10n/generated/quill_localizations_lt.dart
@@ -0,0 +1,338 @@
+import 'quill_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Lithuanian (`lt`).
+class FlutterQuillLocalizationsLt extends FlutterQuillLocalizations {
+  FlutterQuillLocalizationsLt([String locale = 'lt']) : super(locale);
+
+  @override
+  String get pasteLink => 'Įklijuoti nuorodą';
+
+  @override
+  String get ok => 'Gerai';
+
+  @override
+  String get selectColor => 'Pasirinkti spalvą';
+
+  @override
+  String get gallery => 'Galerija';
+
+  @override
+  String get link => 'Nuoroda';
+
+  @override
+  String get open => 'Atidaryti';
+
+  @override
+  String get copy => 'Kopijuoti';
+
+  @override
+  String get remove => 'Pašalinti';
+
+  @override
+  String get save => 'Išsaugoti';
+
+  @override
+  String get zoom => 'Priartinti';
+
+  @override
+  String get saved => 'Išsaugota';
+
+  @override
+  String get text => 'Tekstas';
+
+  @override
+  String get resize => 'Keisti dydį';
+
+  @override
+  String get width => 'Plotis';
+
+  @override
+  String get height => 'Aukštis';
+
+  @override
+  String get size => 'Dydis';
+
+  @override
+  String get small => 'Mažas';
+
+  @override
+  String get large => 'Didelis';
+
+  @override
+  String get huge => 'Milžiniškas';
+
+  @override
+  String get clear => 'Išvalyti';
+
+  @override
+  String get font => 'Šriftas';
+
+  @override
+  String get search => 'Ieškoti';
+
+  @override
+  String get camera => 'Kamera';
+
+  @override
+  String get video => 'Vaizdo įrašas';
+
+  @override
+  String get undo => 'Atšaukti';
+
+  @override
+  String get redo => 'Pakartoti';
+
+  @override
+  String get fontFamily => 'Šrifto šeima';
+
+  @override
+  String get fontSize => 'Šrifto dydis';
+
+  @override
+  String get bold => 'Paryškintas';
+
+  @override
+  String get subscript => 'Apatinis indeksas';
+
+  @override
+  String get superscript => 'Viršutinis indeksas';
+
+  @override
+  String get italic => 'Kursyvas';
+
+  @override
+  String get underline => 'Pabrauktas';
+
+  @override
+  String get strikeThrough => 'Perbrauktas';
+
+  @override
+  String get inlineCode => 'Eilutės kodas';
+
+  @override
+  String get fontColor => 'Šrifto spalva';
+
+  @override
+  String get backgroundColor => 'Fono spalva';
+
+  @override
+  String get clearFormat => 'Išvalyti formatavimą';
+
+  @override
+  String get alignLeft => 'Lygiuoti kairėje';
+
+  @override
+  String get alignCenter => 'Centruoti';
+
+  @override
+  String get alignRight => 'Lygiuoti dešinėje';
+
+  @override
+  String get alignJustify => 'Lygiuoti tekstą';
+
+  @override
+  String get justifyWinWidth => 'Lygiuoti pagal plotį';
+
+  @override
+  String get textDirection => 'Teksto kryptis';
+
+  @override
+  String get headerStyle => 'Antraštės stilius';
+
+  @override
+  String get normal => 'Normalus';
+
+  @override
+  String get heading1 => 'Antraštė 1';
+
+  @override
+  String get heading2 => 'Antraštė 2';
+
+  @override
+  String get heading3 => 'Antraštė 3';
+
+  @override
+  String get heading4 => 'Antraštė 4';
+
+  @override
+  String get heading5 => 'Antraštė 5';
+
+  @override
+  String get heading6 => 'Antraštė 6';
+
+  @override
+  String get numberedList => 'Numeruotas sąrašas';
+
+  @override
+  String get bulletList => 'Ženklintas sąrašas';
+
+  @override
+  String get checkedList => 'Pažymėtas sąrašas';
+
+  @override
+  String get codeBlock => 'Kodo blokas';
+
+  @override
+  String get quote => 'Citata';
+
+  @override
+  String get increaseIndent => 'Didinti įtrauką';
+
+  @override
+  String get decreaseIndent => 'Mažinti įtrauką';
+
+  @override
+  String get insertURL => 'Įterpti URL';
+
+  @override
+  String get visitLink => 'Aplankyti nuorodą';
+
+  @override
+  String get enterLink => 'Įvesti nuorodą';
+
+  @override
+  String get enterMedia => 'Įvesti mediją';
+
+  @override
+  String get edit => 'Redaguoti';
+
+  @override
+  String get apply => 'Pritaikyti';
+
+  @override
+  String get hex => 'Hex';
+
+  @override
+  String get material => 'Medžiaga';
+
+  @override
+  String get color => 'Spalva';
+
+  @override
+  String get lineheight => 'Eilutės aukštis';
+
+  @override
+  String get findText => 'Rasti tekstą';
+
+  @override
+  String get moveToPreviousOccurrence => 'Pereiti prie ankstesnio atvejo';
+
+  @override
+  String get moveToNextOccurrence => 'Pereiti prie kito atvejo';
+
+  @override
+  String get savedUsingTheNetwork => 'Išsaugota naudojant tinklą';
+
+  @override
+  String get savedUsingLocalStorage => 'Išsaugota naudojant vietinę saugyklą';
+
+  @override
+  String theImageHasBeenSavedAt(String imagePath) {
+    return 'Paveikslėlis buvo išsaugotas: $imagePath';
+  }
+
+  @override
+  String get errorWhileSavingImage => 'Klaida išsaugant paveikslėlį';
+
+  @override
+  String get pleaseEnterTextForYourLink =>
+      'Prašome įvesti tekstą jūsų nuorodai (pvz., \'Sužinoti daugiau\')';
+
+  @override
+  String get pleaseEnterTheLinkURL =>
+      'Prašome įvesti nuorodos URL (pvz., \'https://example.com\')';
+
+  @override
+  String get pleaseEnterAValidImageURL =>
+      'Prašome įvesti galiojantį paveikslėlio URL';
+
+  @override
+  String get pleaseEnterAValidVideoURL =>
+      'Prašome įvesti galiojantį vaizdo įrašo URL';
+
+  @override
+  String get photo => 'Nuotrauka';
+
+  @override
+  String get image => 'Paveikslėlis';
+
+  @override
+  String get caseSensitivityAndWholeWordSearch =>
+      'Raidžių jautrumas ir viso žodžio paieška';
+
+  @override
+  String get caseSensitive => 'Jautri raidėms';
+
+  @override
+  String get wholeWord => 'Visas žodis';
+
+  @override
+  String get insertImage => 'Įterpti paveikslėlį';
+
+  @override
+  String get pickAPhotoFromYourGallery => 'Pasirinkti nuotrauką iš galerijos';
+
+  @override
+  String get takeAPhotoUsingYourCamera => 'Nufotografuoti kamera';
+
+  @override
+  String get pasteAPhotoUsingALink => 'Įklijuoti nuotrauką naudojant nuorodą';
+
+  @override
+  String get pickAVideoFromYourGallery =>
+      'Pasirinkti vaizdo įrašą iš galerijos';
+
+  @override
+  String get recordAVideoUsingYourCamera => 'Įrašyti vaizdo įrašą kamera';
+
+  @override
+  String get pasteAVideoUsingALink =>
+      'Įklijuoti vaizdo įrašą naudojant nuorodą';
+
+  @override
+  String get close => 'Uždaryti';
+
+  @override
+  String get searchSettings => 'Paieškos nustatymai';
+
+  @override
+  String get cut => 'Iškirpti';
+
+  @override
+  String get paste => 'Įklijuoti';
+
+  @override
+  String get insertTable => 'Įterpti lentelę';
+
+  @override
+  String get insertVideo => 'Įterpti vaizdo įrašą';
+
+  @override
+  String get errorUnexpectedSavingImage =>
+      'Netikėta klaida išsaugant paveikslėlį';
+
+  @override
+  String get successImageSavedGallery =>
+      'Paveikslėlis sėkmingai išsaugotas galerijoje';
+
+  @override
+  String get successImageSaved => 'Paveikslėlis sėkmingai išsaugotas';
+
+  @override
+  String get successImageDownloaded => 'Paveikslėlis sėkmingai atsisiųstas';
+
+  @override
+  String get openGallery => 'Atidaryti galeriją';
+
+  @override
+  String get openFileLocation => 'Atidaryti failo vietą';
+
+  @override
+  String get openFile => 'Atidaryti failą';
+
+  @override
+  String get saveImagePermissionDenied =>
+      'Leidimas išsaugoti paveikslėlį buvo atmestas';
+}
