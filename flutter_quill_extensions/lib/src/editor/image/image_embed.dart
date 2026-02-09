a3d8d0ef7f35bd6cad16270f8a89d1b1e05b1800
tam nguyen <tam.nguyen4@sotatek.com>
2026-02-09 16:55:07 +0700

    revert ontap

--- flutter_quill_extensions/lib/src/editor/image/image_embed.dart
+++ flutter_quill_extensions/lib/src/editor/image/image_embed.dart
@@ -43,7 +43,7 @@ class QuillEditorImageEmbedBuilder extends EmbedBuilder {
     );
 
     return GestureDetector(
-      onLongPress: () {
+      onTap: () {
         final onImageClicked = config.onImageClicked;
         if (onImageClicked != null) {
           onImageClicked(imageSource);
